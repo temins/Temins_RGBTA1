5ed22add103fca85d78c93c7a72255d620df8c44
temins <98459093+temins@users.noreply.github.com>
2022-03-10 21:53:40 +0700

    Update get_Value.ino

--- examples/get_Value/get_Value.ino
+++ examples/get_Value/get_Value.ino
@@ -1,6 +1,6 @@
 #include <Temins_RGTBA1.h>
 
-Temins_RGTBA1 TB(2, 0.042, 0.005024, 2); //pin, volume in litre, surface area in meter, reset time in minute
+Temins_RGTBA1 TB(2, 0.025, 0.005024, 2); //pin, volume in litre, surface area in meter, reset time in minute
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,10 +10,13 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   int tip = TB.getTip();
+  double Volume = TB.getVolumeNow();
   double RF = TB.getRainFallNow();
   double LastRF = TB.getLastRainFall();
   Serial.print("Tip : ");
   Serial.println(tip);
+  Serial.print("Volume : ");
+  Serial.println(Volume);
   Serial.print("RF : ");
   Serial.print(RF);
   Serial.println(" mm");
